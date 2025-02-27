41a8c46f0857f911c30a27749d310f6b0bf0cfb9
RodrigVF <rod.vf@hotmail.com>
2025-02-27 20:08:17 -0300

    feat: Add migration V1__create_tb_events.sql

---
+++ src/main/resources/db/migration/V1__create_tb_events.sql
@@ -0,0 +1,16 @@
+--V1_create_tb_events.sql
+CREATE TABLE tb_events (
+    id BIGINT PRIMARY KEY,
+    identifier VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    organizer VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    max_participants INTEGER NOT NULL,
+    start_date TIMESTAMP NOT NULL,
+    end_date TIMESTAMP NOT NULL,
+    description TEXT,
+    game VARCHAR(255),
+    banner_img_url VARCHAR(255),
+    stream_url VARCHAR(255),
+    discord_url VARCHAR(255)
+);
